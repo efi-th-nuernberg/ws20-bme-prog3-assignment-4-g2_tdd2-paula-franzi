c80d6184426e262fa96208ba988ec212db8e6e5b
repl.it user <replituser@example.com>
2020-11-05 18:26:53 +0000

    Assignment 4 5.11.2020

--- TriangleChecker.java
+++ TriangleChecker.java
@@ -1,14 +1,11 @@
 import java.util.Scanner;
 
 public class TriangleChecker {
-  
+
   public enum TriangleType {
-    NONE, 
-    NORMAL,
-    ISOSCELES,  // Gleichschenklig
+    NONE, NORMAL, ISOSCELES, // Gleichschenklig
     EQUILATERAL // Gleichseitig
   }
-
 
   public static void main(String[] args) {
     Scanner s = new Scanner(System.in);
@@ -30,37 +27,46 @@ public class TriangleChecker {
   private static void printAnalysis(float a, float b, float c) {
     TriangleType type = checkTriangle(a, b, c);
     switch (type) {
-      case NONE:
-        System.out.println("Kein Dreieck");
-        break;        
-      case NORMAL:
-        System.out.println("Dreieck");
-        break;        
-      case ISOSCELES:
-        System.out.println("Gleichschenkliges Dreieck");
-        break;        
-      case EQUILATERAL:
-        System.out.println("Gleichseitiges Dreieck");
-        break;        
+    case NONE:
+      System.out.println("Kein Dreieck");
+      break;
+    case NORMAL:
+      System.out.println("Dreieck");
+      break;
+    case ISOSCELES:
+      System.out.println("Gleichschenkliges Dreieck");
+      break;
+    case EQUILATERAL:
+      System.out.println("Gleichseitiges Dreieck");
+      break;
     }
   }
 
   // Analyse der Dreiecksart
   public static TriangleType checkTriangle(float a, float b, float c) {
+    // Seitenlängen <= 0
+    if (a <= 0 || b <= 0 || c <= 0) {
+      return TriangleType.NONE;
+    }
+    // Summe zweier Seiten <= dritte Seite oder dass eine Seite zu lang ist
+    if ((a + b) <= c || (b + c) <= a || (a + c) <= b) {
+      return TriangleType.NONE;
+    }
 
-    if((a == b) && (b == c)){
+    // mehr Bedingungen für "NONE" fallen mir nicht ein
+
+    if ((a == b) && (b == c)) {
       return TriangleType.EQUILATERAL;
     }
 
-    if ((a == b || a ==c) && (b != c)){
+    if ((a == b || a == c) && (b != c)) {
       return TriangleType.ISOSCELES;
     }
 
-    if((a != b) && (b != c)){
+    if ((a != b) && (b != c)) {
       return TriangleType.NORMAL;
     }
     return TriangleType.NONE;
   }
-
 
 }
--- TriangleCheckerTest.java
+++ TriangleCheckerTest.java
@@ -2,21 +2,120 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class TriangleCheckerTest {
-    
-    @Test
-    public void testValidTriangle() {
 
-        // Arrange
-        float a = 2;
-        float b = 3;
-        float c = 4;
+  @Test
+  public void testValidTriangle() {
 
-        // Act
-        TriangleChecker.TriangleType type = TriangleChecker.checkTriangle(a, b, c);
+    // Arrange
+    float a = 2;
+    float b = 3;
+    float c = 4;
 
-        // Assert
-        assertEquals(TriangleChecker.TriangleType.NORMAL, type);
+    // Act
+    TriangleChecker.TriangleType type = TriangleChecker.checkTriangle(a, b, c);
 
-    }
+    // Assert
+    assertEquals(TriangleChecker.TriangleType.NORMAL, type);
+
+  }
+
+  @Test
+  public void testEquilateralTriangle() {
+
+    // Arrange
+    float a = 2;
+    float b = 2;
+    float c = 2;
+
+    // Act
+    TriangleChecker.TriangleType type = TriangleChecker.checkTriangle(a, b, c);
+
+    // Assert
+    assertEquals("Test-Case:Is eqilateral", TriangleChecker.TriangleType.EQUILATERAL, type);
+    /*
+     * b = 3;
+     * 
+     * type = TriangleChecker.checkTriangle(a, b, c);
+     * 
+     * assertNotEquals("Test-Case:Is not eqilateral",TriangleChecker.TriangleType.
+     * EQUILATERAL, type);
+     */
+  }
+
+  @Test
+  public void testInvalidTriangle() {
+
+    // 1. Arrange (a is negative)
+    float a = -2;
+    float b = 3;
+    float c = 4;
+
+    // Act
+    TriangleChecker.TriangleType type = TriangleChecker.checkTriangle(a, b, c);
+
+    // Assert
+    assertEquals("Test-Case:Is invalid ( a = negative number)", TriangleChecker.TriangleType.NONE, type);
+
+    // 2. Arrange (b is negative)
+    a = 2;
+    b = -3;
+    c = 4;
+    type = TriangleChecker.checkTriangle(a, b, c);
+
+    // Assert
+    assertEquals("Test-Case:Is invalid (b = negative number)", TriangleChecker.TriangleType.NONE, type);
+
+    // 3. Arrange (c is negative)
+    a = 2;
+    b = 3;
+    c = -4;
+    type = TriangleChecker.checkTriangle(a, b, c);
+
+    // Assert
+    assertEquals("Test-Case:Is invalid (negative Number)", TriangleChecker.TriangleType.NONE, type);
+
+    // 4. Arrange (c >= sum of a & b)
+    a = 2;
+    b = 3;
+    c = 40;
+    type = TriangleChecker.checkTriangle(a, b, c);
+
+    // Assert
+    assertEquals("Test-Case:Is invalid (c >=  sum of a & b)", TriangleChecker.TriangleType.NONE, type);
+
+    // 5. Arrange (b >= sum of a & c)
+    a = 2;
+    b = 30;
+    c = 4;
+    type = TriangleChecker.checkTriangle(a, b, c);
+
+    // Assert
+    assertEquals("Test-Case:Is invalid (b >=  sum of a & c)", TriangleChecker.TriangleType.NONE, type);
+
+    // 6. Arrange (a >= sum of b & c)
+    a = 20;
+    b = 3;
+    c = 4;
+    type = TriangleChecker.checkTriangle(a, b, c);
+
+    // Assert
+    assertEquals("Test-Case:Is invalid (a >=  sum of b & c)", TriangleChecker.TriangleType.NONE, type);
+  }
+
+  @Test
+  public void testIsoscelesTriangle() {
+
+    // Arrange
+    float a = 3;
+    float b = 3;
+    float c = 4;
+
+    // Act
+    TriangleChecker.TriangleType type = TriangleChecker.checkTriangle(a, b, c);
+
+    // Assert
+    assertEquals(TriangleChecker.TriangleType.ISOSCELES, type);
+
+  }
 
 }
